7a58d5bef208b0127c2089e60bd8dbf6860820aa
lucas vasconcelos <lks.lucasvasconcelos@gmail.com>
2023-08-07 03:21:12 -0300

    first commit

---
+++ src/main/java/com/payment/proportionalpaymentservice/ProportionalPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.payment.proportionalpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProportionalPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProportionalPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payment/proportionalpaymentservice/ProportionalPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.proportionalpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProportionalPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
